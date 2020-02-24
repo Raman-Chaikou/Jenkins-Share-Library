dcccd6dacee96dd69bd3a8b6686cf62fc588d97b
Raman <raman.chaikou@oeconnection.com>
2020-02-24 12:08:27 +0300

    call a method from method

--- vars/modasStoreDeployedVersions.groovy
+++ vars/modasStoreDeployedVersions.groovy
@@ -7,14 +7,14 @@ def call(pipelineParams, deploymentStatus) {
     }
     def versionFileName = "/var/jenkins_home/tmp/modas/modas-${environment}.versions";
 
-    storeVersion(versionFileName, 'modas/modas_frontend', pipelineParams.GUI_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/modas_backend_releasing', pipelineParams.RELEASING_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/modas_backend_cataloguing', pipelineParams.CATALOGUING_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/modas_backend_illustrations', pipelineParams.ATTACHMENTS_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/modas_backend_descriptions', pipelineParams.DESCRIPTIONS_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/waf', pipelineParams.WAF_VERSION, deploymentStatus)
-    storeVersion(versionFileName, 'modas/lb', pipelineParams.LB_VERSION, deploymentStatus)    
-    storeVersion(versionFileName, 'ecat/keycloak', pipelineParams.KEYCLOAK_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/modas_frontend', pipelineParams.GUI_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/modas_backend_releasing', pipelineParams.RELEASING_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/modas_backend_cataloguing', pipelineParams.CATALOGUING_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/modas_backend_illustrations', pipelineParams.ATTACHMENTS_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/modas_backend_descriptions', pipelineParams.DESCRIPTIONS_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/waf', pipelineParams.WAF_VERSION, deploymentStatus)
+    modasStoreDeployedVersion(versionFileName, 'modas/lb', pipelineParams.LB_VERSION, deploymentStatus)    
+    modasStoreDeployedVersion(versionFileName, 'ecat/keycloak', pipelineParams.KEYCLOAK_VERSION, deploymentStatus)
 
     println "--------------------------\nDeployment info: ${environment}\n${readFile(versionFileName)}\n--------------------------"
 }
